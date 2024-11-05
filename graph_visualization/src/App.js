710fdfaa103d00f2dd9b2a932eeb4c0c495a5713
unknown <gauravkadyan321@gmail.com>
2024-11-05 11:32:07 +0530

    visualization and api integration

--- graph_visualization/src/App.js
+++ graph_visualization/src/App.js
@@ -1,8 +1,20 @@
-import logo from './logo.svg';
-import Dashboard from './dashboard';
+import React, { useState } from 'react';
+import Sidebar from './components/Sidebar';
+import GitHub from './components/GitHubPage';
 
 function App() {
-  return <Dashboard/>;
+  const [activePage, setActivePage] = useState('github');
+
+  return (
+    <div className="app-container">
+      <Sidebar setActivePage={setActivePage} />
+      <div className="content">
+        {activePage === 'github' && <GitHub />}
+        {activePage === 'linkedin' && <h1>LinkedIn Page - Coming Soon!</h1>}
+        {activePage === 'twitter' && <h1>Twitter Page - Coming Soon!</h1>}
+      </div>
+    </div>
+  );
 }
 
 export default App;
